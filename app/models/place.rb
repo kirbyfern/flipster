e715117fce6af20a38fac77d187d32afeb7eb4bb
kboygit <kirbyy.james@gmail.com>
2017-03-20 00:06:23 +0000

    creates a validation for all params

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
+    validates :name, length: { minimum: 3, too_short: "%{count} characters is the minimum" }, presence: true
+    validates :address, length: { minimum: 5, too_short: "%{count} characters is the minimum"}, presence: true
+    validates :description, length: { minimum: 10, too_short: "%{count} characters is the minimum" } , presence: true
 end
