b57759ca20fba54161fad4555ce7350e74a2530d
kboygit <kirbyy.james@gmail.com>
2017-03-24 23:05:14 +0000

    integrated carrierwave, photo db, styled

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,11 +3,11 @@ class Comment < ApplicationRecord
     belongs_to :place
 
     RATINGS = {
-        'one star': '1_star',
-        'two stars': '2_stars',
-        'three stars': '3_stars',
-        'four stars': '4_stars',
-        'five stars': '5_stars'
+        '🌟': '🌟',
+        '🌟🌟': '🌟🌟',
+        '🌟🌟🌟': '🌟🌟🌟',
+        '🌟🌟🌟🌟': '🌟🌟🌟🌟',
+        '🌟🌟🌟🌟🌟': '🌟🌟🌟🌟🌟'
     }
 
     def humanized_rating
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324042957) do
+ActiveRecord::Schema.define(version: 20170324181306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,13 @@ ActiveRecord::Schema.define(version: 20170324042957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
-    t.text     "description"
     t.string   "address"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
